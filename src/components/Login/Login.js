641baae2d120198424803a37e13882b65225605f
singhjagtar0290 <singhjagtar0290@gmail.com>
2022-05-30 00:16:13 +0530

    Use reducers

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,8 +5,8 @@ import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
