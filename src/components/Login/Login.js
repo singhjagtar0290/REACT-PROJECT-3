06e8b32936328539412f45440485f6d1b4f74c0b
singhjagtar0290 <singhjagtar0290@gmail.com>
2022-05-29 23:45:37 +0530

    St timeout cleanup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,10 +12,19 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout( () => {
+      console.log('checking form validity')
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('CLEANUP')
+      clearTimeout(identifier)
+    }
   }, [enteredEmail,enteredPassword])
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
   };
